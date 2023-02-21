38ce6deee8c343bed63ed74605bd180ca081f174
Dasa Krepsova <dagmar.krepsova@seznam.cz>
2023-02-21 20:50:36 +0100

    Ćást 1 - pracovni verze 1 - trojuhelnik

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,54 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
-    private Turtle zofka;
+    private Turtle zofka = new Turtle();
 
     public void start() {
-        zofka = new Turtle();
+
+        nakresliRovnostrannyTrojuhelnik (80, Color.red);
+
     }
+
+
+    public void nakresliRovnostrannyTrojuhelnik (double delkaStrany, Color barva) {
+        zofka.turnRight(90);
+
+        zofka.setPenColor(barva);
+        zofka.move(delkaStrany);
+        zofka.turnLeft(120.0);
+        zofka.move(delkaStrany);
+        zofka.turnLeft(120.0);
+        zofka.move(delkaStrany);
+        zofka.turnLeft(120.0);
+    }
+
+//    Turtle zofka;
+//
+//    public static void main(String[]args) {
+//        zofka = new Turtle();
+//
+//        zofka.setLocation(100.0,100.0);
+//        // Volani metody, do ktere se preda hodnota 50.0
+//        nakresliRovnostrannyTrojuhelnik(50.0);
+//
+//        zofka.setLocation(300.0,100.0);
+//        // Volani metody, do ktere se preda hodnota 70.0
+//        nakresliRovnostrannyTrojuhelnik(70.0);
+//    }
+//
+//    public void nakresliRovnostrannyTrojuhelnik(double velikostStrany) {
+//        // Zde lze používat proměnnou velikostStrany.
+//        // Jeji hodnota je takova, s jakou byla metoda zavolana
+//        zofka.move(velikostStrany);
+//        zofka.turnLeft(120.0);
+//        zofka.move(velikostStrany);
+//        zofka.turnLeft(120.0);
+//        zofka.move(velikostStrany);
+//        zofka.turnLeft(120.0);
+//    }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
