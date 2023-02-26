33e069b0c61a179c105252394b66dc7ef3f6da1e
Dasa Krepsova <dagmar.krepsova@seznam.cz>
2023-02-27 00:14:54 +0100

    Úkol 2 - Část 2 - zofka kresli kazdou vec zvlast

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,21 +9,83 @@ public class HlavniProgram {
 
     public void start() {
 
-        zofka.setLocation(300.0,300.0);
-        nakresliRovnostrannyTrojuhelnik(70.0, Color.blue);
+//        Cast 2 / zofka kresli kazdou vec zvlast
 
-        zofka.setLocation(400.0,300.0);
-        nakresliCtverec(70.0, Color.red);
+//        Zofka kresli zmrzlinu
 
-        zofka.setLocation(570.0,300.0);
-        nakresliObdelnik(70.0, 120, Color.yellow);
+        zofka.setLocation(300,300);
+        nakresliZmrzlinu(Color.blue);
 
-        zofka.setLocation(820.0,300.0);
-        nakresliKolecko(8, Color.green);
+//        Zofka kresli snehulaka
+
+//        zofka.setLocation(500,300);
+//        nakresliKolecko(5, Color.red);
+//        zofka.setLocation(475, 170);
+//        nakresliKolecko(8, Color.red);
+//        zofka.setLocation(450, 420);
+//        nakresliKolecko(4, Color.red);
+//        zofka.setLocation(420, 260);
+//        nakresliKolecko(10, Color.red);
+//        zofka.setLocation(580,210);
+//        nakresliKolecko(10, Color.red);
+
+//        zofka kresli masinku
+
+//        zofka.setLocation(600, 400);
+//        nakresliRovnostrannyTrojuhelnik(100, Color.green);
+//        zofka.penUp();
+//        zofka.move(40);
+//        zofka.penDown();
+//        nakresliObdelnik(100, 200, Color.green);
+//        zofka.penUp();
+//        zofka.turnRight(90);
+//        zofka.move(300);
+//        zofka.turnRight(180);
+//        zofka.penDown();
+//        nakresliObdelnik(100, 200, Color.green);
+//        nakresliKolecko(6, Color.green);
+//        zofka.setLocation(630,390);
+//        nakresliKolecko(8, Color.green);
+//        zofka.setLocation(720,395);
+//        nakresliKolecko(8, Color.green);
+
+//        Cast 1 / zakladni obrazce
+
+//        zofka.setLocation(300.0,300.0);
+//        nakresliRovnostrannyTrojuhelnik(70.0, Color.blue);
+//
+//        zofka.setLocation(400.0,300.0);
+//        nakresliCtverec(70.0, Color.red);
+//
+//        zofka.setLocation(570.0,300.0);
+//        nakresliObdelnik(70.0, 120, Color.yellow);
+//
+//        zofka.setLocation(820.0,300.0);
+//        nakresliKolecko(8, Color.green);
+    }
+
+    public void nakresliZmrzlinu (Color barva) {
+        zofka.setPenColor(barva);
+        zofka.penUp();
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.penDown();
+        zofka.move(42);
+        zofka.turnRight(110);
+        zofka.move(130);
+        zofka.turnRight(140);
+        zofka.move(130);
+        zofka.turnRight(110);
+        zofka.move(42);
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(42);
+        zofka.turnRight(180);
+        zofka.penDown();
+        nakresliKolecko(7, barva);
     }
 
     public void nakresliRovnostrannyTrojuhelnik (double delkaStrany, Color barva) {
-
         zofka.setPenColor(barva);
         zofka.move(delkaStrany);
         zofka.turnLeft(120.0);
@@ -34,7 +96,6 @@ public class HlavniProgram {
     }
 
     public void nakresliCtverec (double delkaStrany, Color barva) {
-
         zofka.setPenColor(barva);
         zofka.move(delkaStrany);
         zofka.turnRight(90);
@@ -47,7 +108,6 @@ public class HlavniProgram {
     }
 
     public void nakresliObdelnik (double delkaStranyA, double delkaStranyB, Color barva) {
-
         zofka.setPenColor(barva);
         zofka.move(delkaStranyA);
         zofka.turnRight(90);
