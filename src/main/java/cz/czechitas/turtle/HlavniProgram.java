0df3a38636c22259d29d83a0ff680e53f3e5f30b
Dasa Krepsova <dagmar.krepsova@seznam.cz>
2023-02-26 21:42:30 +0100

    Úkol 2 - Část 1.1 - metody na kreslení základních tvarů

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -9,19 +9,18 @@ public class HlavniProgram {
 
     public void start() {
 
-
-//        nakresliRovnostrannyTrojuhelnik (80, Color.red);
-
-
-        zofka.setLocation(200.0,200.0);
-        nakresliRovnostrannyTrojuhelnik(50.0, Color.red);
-
-        zofka.setLocation(300.0,100.0);
+        zofka.setLocation(300.0,300.0);
         nakresliRovnostrannyTrojuhelnik(70.0, Color.blue);
 
+        zofka.setLocation(400.0,300.0);
+        nakresliCtverec(70.0, Color.red);
 
+        zofka.setLocation(570.0,300.0);
+        nakresliObdelnik(70.0, 120, Color.yellow);
+
+        zofka.setLocation(820.0,300.0);
+        nakresliKolecko(8, Color.green);
     }
-
 
     public void nakresliRovnostrannyTrojuhelnik (double delkaStrany, Color barva) {
 
@@ -34,30 +33,40 @@ public class HlavniProgram {
         zofka.turnLeft(120.0);
     }
 
-//    Turtle zofka;
-//
-//    public static void main(String[]args) {
-//        zofka = new Turtle();
-//
-//        zofka.setLocation(100.0,100.0);
-//        // Volani metody, do ktere se preda hodnota 50.0
-//        nakresliRovnostrannyTrojuhelnik(50.0);
-//
-//        zofka.setLocation(300.0,100.0);
-//        // Volani metody, do ktere se preda hodnota 70.0
-//        nakresliRovnostrannyTrojuhelnik(70.0);
-//    }
-//
-//    public void nakresliRovnostrannyTrojuhelnik(double velikostStrany) {
-//        // Zde lze používat proměnnou velikostStrany.
-//        // Jeji hodnota je takova, s jakou byla metoda zavolana
-//        zofka.move(velikostStrany);
-//        zofka.turnLeft(120.0);
-//        zofka.move(velikostStrany);
-//        zofka.turnLeft(120.0);
-//        zofka.move(velikostStrany);
-//        zofka.turnLeft(120.0);
-//    }
+    public void nakresliCtverec (double delkaStrany, Color barva) {
+
+        zofka.setPenColor(barva);
+        zofka.move(delkaStrany);
+        zofka.turnRight(90);
+        zofka.move(delkaStrany);
+        zofka.turnRight(90);
+        zofka.move(delkaStrany);
+        zofka.turnRight(90);
+        zofka.move(delkaStrany);
+        zofka.turnRight(90);
+    }
+
+    public void nakresliObdelnik (double delkaStranyA, double delkaStranyB, Color barva) {
+
+        zofka.setPenColor(barva);
+        zofka.move(delkaStranyA);
+        zofka.turnRight(90);
+        zofka.move(delkaStranyB);
+        zofka.turnRight(90);
+        zofka.move(delkaStranyA);
+        zofka.turnRight(90);
+        zofka.move(delkaStranyB);
+        zofka.turnRight(90);
+    }
+
+    public void nakresliKolecko (int uhel, Color barva) {
+        zofka.turnLeft(90);
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 100; i++) {
+                zofka.move(5);
+                zofka.turnRight(uhel);
+        }
+    }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
